01e4afa644b3d75e697a06a28388147c49f94cca
JonataBecker <jonatabecker@gmail.com>
2017-03-28 23:19:42 -0300

    Comando para esquerda

--- src/Animation.java
+++ src/Animation.java
@@ -4,17 +4,19 @@
  */
 
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 import javax.imageio.ImageIO;
+import javax.swing.JComponent;
 
 /**
  * Descrição da classe.
  */
 public class Animation {
 
-    private final List<BufferedImage> frames;
+    private final List<Image> frames;
 
     private int frame = 0;
 
@@ -24,6 +26,8 @@ public class Animation {
         try {
             BufferedImage buff = ImageIO.read(getClass().getResource("sprite.png"));
 
+            
+            
             frames.add(buff.getSubimage(4, 22, 75, 140));
             frames.add(buff.getSubimage(100, 0, 75, 140));
             frames.add(buff.getSubimage(200, 0, 75, 140));
@@ -36,8 +40,10 @@ public class Animation {
 
     }
 
-    public void draw(Graphics2D g2d, int x, int y) {
-        g2d.drawImage(frames.get(frame), null, x, y);
+    public void draw(Graphics2D g2d, int x, int y, JComponent component) {
+        g2d.drawRect(x, y, 100, 100);
+
+//        g2d.drawImage(frames.get(frame), x, y, component);
         frame++;
         if (frame >= frames.size()) {
             frame = 0;
--- src/KombatComponent.java
+++ src/KombatComponent.java
@@ -4,14 +4,17 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
-import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /**
  *
  * @author JonataBecker
  */
-public class KombatComponent extends JComponent {
+public class KombatComponent extends JPanel {
 
     private World world;
     private Image runRight;
@@ -22,15 +25,30 @@ public class KombatComponent extends JComponent {
         this.world = world;
         initImages();
 
+        
+        Timer timer = new Timer(175, new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                
+                repaint();
+            }
+        });
+        timer.start();
+        
+        
         Thread th = new Thread(() -> {
             try {
                 while (true) {
-                    repaint();
+//                    SwingUtilities.invokeLater(() -> {
+                        updateUI();
+//                    });
                     Thread.sleep(1000 / 12);
                 }
             } catch (Exception e) {
             }
         });
+        th.setDaemon(true);
         th.start();
     }
 
@@ -48,12 +66,13 @@ public class KombatComponent extends JComponent {
     }
 
     @Override
-    public void paint(Graphics g) {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(Color.red);
         world.getPlayers().forEach((player) -> {
-//            animation.draw(g2d, player.getX(), player.getY());
-            g.drawImage(runRight, player.getX(), player.getY(), this);
+            animation.draw(g2d, player.getX(), player.getY(), this);
+            System.out.println(System.currentTimeMillis());
         });
     }
 
